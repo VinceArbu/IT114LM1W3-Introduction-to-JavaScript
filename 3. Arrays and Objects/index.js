b9bf4053eb75ccd714d68656402f0b093c78b182
VinceArbu <145641054+VinceArbu@users.noreply.github.com>
2024-01-15 15:49:20 +0000

    Exercise #3: DONE

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -53,20 +53,66 @@ const IT114L = {
 
 // Todo 3.4 Add professor name as one of the fields of the object. Display the value of professor name.
 // Your code here
+
+/* 
 IT114L.professorName = "Job Lipat";
 
 console.log("My Professor: " + IT114L.professorName);
+*/
+
 // Todo 3.5 Declare and array of objects with information about the courses you are taking this term
 // Your code here
+let courses = [
+    {
+        courseCode: "CS107",
+        courseName: "Information Management",
+        instructor: "Prof. Dahlia",
+        credits: 2
+    },
+    {
+        courseCode: "IT114",
+        courseName: "Web Systems and Technologies",
+        instructor: "Prof. Ilao",
+        credits: 2
+    },
+    {
+        courseCode: "CS120",
+        courseName: "Structures of Programming Languages",
+        instructor: "Prof. Aurelia",
+        credits: 3
+    },
+];
 
 // Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
 // Your code here
+/*
+let totalUnits = 0;
+for (let i=0; i< courses.length;i++){
+    totalUnits += courses[i].credits;
+}
+
+console.log("Total Units Enrolled this term: " + totalUnits);
+*/
 
 // Checkpoint 3.2 What would be the equivalent of objects in python? How would you compare them?
-// Answer: 
+// Answer: The equivalent of objects in Python would be the dictionaries. Both objects in other
+// programming language JavaScript and dictionaries in Python are key-value pairs, allowing
+// the programmer to associate data with specific names or keys.
 
 // Todo 3.6 Going back to the array of numbers, use the spread syntax to create a copy of the array with an additional number
 // Your code here
+/* 
+let spreadNumbers = [...numbers, 99];
 
+console.log(numbers);
+console.log("\nNew Number Array:", spreadNumbers);
+*/
 // Todo 3.7 Going back to your IT114L object, extract the course code and units using the spread operator
 // Your code here
+/*
+const { courseCode, units } = { ...IT114L };
+
+console.log("Extracted Objects:");
+console.log(`Course Code: ${courseCode}`);
+console.log(`Units: ${units}`);
+ */
