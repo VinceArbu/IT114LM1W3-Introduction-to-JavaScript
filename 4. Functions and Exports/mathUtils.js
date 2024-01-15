e6ca0bce7805344d5a0813d8343a61179c13b86d
VinceArbu <145641054+VinceArbu@users.noreply.github.com>
2024-01-15 16:11:55 +0000

    Exercise 4: Created functions for main index

--- 4. Functions and Exports/mathUtils.js
+++ 4. Functions and Exports/mathUtils.js
@@ -1,7 +1,18 @@
 
 // Todo 5.4.1 Create a function called add that takes in two numbers and returns their sum
-
+/* 
+function add(a, b) {
+    return a + b;
+  }
+*/
 // Todo 5.4.2 Create a constant that contains the approximate value of PI 
+//const PI = Math.PI;
 
 // Todo 5.4.3 Export the function and constant
+export function add(a, b) {
+    return a + b;
+}
+
+export const PI = Math.PI;
+
 // Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
